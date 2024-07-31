07f34939ff069eeb9de2971f83dc1c2b7a65b26f
nicakp <91095824+nicakp@users.noreply.github.com>
2024-07-31 19:05:47 +0630

    Update stage1.sh
    Change for bookworm from buster only for pi4

--- stage1.sh
+++ stage1.sh
@@ -19,34 +19,33 @@ echo "proc            /proc           proc    defaults          0       0
 
 echo "debian-rpi64" >> /mnt/etc/hostname
 
-echo "deb http://deb.debian.org/debian/ buster main contrib non-free
-#deb-src http://deb.debian.org/debian/ buster main contrib non-free
+echo "deb http://deb.debian.org/debian/ bookworm main contrib non-free
+#deb-src http://deb.debian.org/debian/ bookworm main contrib non-free
 
-deb http://deb.debian.org/debian/ buster-updates main contrib non-free
-#deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free
+#deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free
 
-deb http://deb.debian.org/debian-security buster/updates main contrib non-free
-#deb-src http://deb.debian.org/debian-security buster/updates main contrib non-free
+deb http://deb.debian.org/debian-security bookworm/updates main contrib non-free
+#deb-src http://deb.debian.org/debian-security bookworm/updates main contrib non-free
 
-deb http://ftp.debian.org/debian buster-backports main
-#deb-src http://ftp.debian.org/debian buster-backports main" >> /mnt/etc/apt/sources.list
+deb http://ftp.debian.org/debian bookworm-backports main
+#deb-src http://ftp.debian.org/debian bookworm-backports main" >> /mnt/etc/apt/sources.list
 
 # Setup bootloader and kernel
 
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-bootloader_1.20190925-2_armhf.deb
-mkdir /tmp/pi-bootloader/
+wget http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/raspberrypi-bootloader_1.20230509~buster-1_armhf.deb
 dpkg-deb -x raspberrypi-bootloader_1.20190925-2_armhf.deb /tmp/pi-bootloader/
 cp /tmp/pi-bootloader/boot/* /mnt/boot/
-rm raspberrypi-bootloader_1.20190925-2_armhf.deb
+rm raspberrypi-bootloader_1.20230509~buster-1_armhf.deb
 
-wget https://github.com/sakaki-/bcm2711-kernel/releases/download/4.19.93.20200107/bcm2711-kernel-4.19.93.20200107.tar.xz
+wget https://github.com/sakaki-/bcm2711-kernel/releases/download/5.4.69.20201006/bcm2711-kernel-5.4.69.20201006.tar.xz
 mkdir /tmp/pi-kernel
-tar xf bcm2711-kernel-4.19.93.20200107.tar.xz -C /tmp/pi-kernel/
+tar xf bcm2711-kernel-5.4.69.20201006.tar.xz -C /tmp/pi-kernel/
 cp -r /tmp/pi-kernel/boot/* /mnt/boot/
 mv /mnt/boot/kernel*.img /mnt/boot/kernel8.img
 mkdir /mnt/lib/modules
 cp -r /tmp/pi-kernel/lib/modules /mnt/lib/
-rm bcm2711-kernel-4.19.93.20200107.tar.xz
+rm bcm2711-kernel-5.4.69.20201006.tar.xz
 rm -r /tmp/pi-kernel
 
 ## Comment or remove completely the above kernel setup and uncomment the kernel setup below to setup the image to run on the Pi 3 instead of the Pi 4.
